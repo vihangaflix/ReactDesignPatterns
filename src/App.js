8c803aaa0067d399a96812ca6e81ddcd60658e04
Vihanga Pramudith <it20192082@my.sliit.lk>
2023-05-11 19:09:29 +0530

    Loading data from localStorage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import { ProductInfo } from "./ProductInfo";
 import ResourceLoader from "./ResourceLoader";
 import UserInfo from "./UserInfo";
+import DataSource from "./DataSource";
+import axios from "axios";
+
+const getServerData = url => async () => {
+    const response = await axios.get(url);
+    return response.data;
+}
+
+const getLocalStorageData = key => () => {
+    return localStorage.getItem(key);
+}
+
+const Text = ({ message }) => <h1>{message}</h1>
 
 function App() {
-	return (
-    <>
-	  <ResourceLoader resourceUrl="/users/234" resourceName="user">
-        <UserInfo />
-      </ResourceLoader>
-      <ResourceLoader resourceUrl="/products/1234" resourceName="product">
-        <ProductInfo />
-      </ResourceLoader>
-    </>
+    return (
+        <>
+            <DataSource getDataFunc={getServerData("/users/234")} resourceName="user">
+                <UserInfo />
+            </DataSource>
+            <DataSource getDataFunc={getLocalStorageData("message")} resourceName="message">
+                <Text />
+            </DataSource>
+        </>
     );
 }
 
