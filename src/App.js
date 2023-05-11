a3226818b3e4c44719b1ade4d6512574e8ccb2bd
Vihanga Pramudith <it20192082@my.sliit.lk>
2023-05-11 18:50:51 +0530

    userLoader and resourceLoader

--- src/App.js
+++ src/App.js
@@ -1,51 +1,16 @@
-import Modal from "./Modal";
-import NumberedList from "./NumberedList";
-import RegularList from "./RegularList";
-import LargePersonListItem from "./people/LargePersonListItem";
-import SmallPersonListItem from "./people/SmallPersonListItem";
-import LargeProductListItem from "./product/LargeProductListItem";
-import SmallProductListItem from "./product/SmallProductListItem";
-
-const people = [{
-	name: 'John Doe',
-	age: 54,
-	hairColor: 'brown',
-	hobbies: ['swimming', 'bicycling', 'video games'],
-}, {
-	name: 'Brenda Smith',
-	age: 33,
-	hairColor: 'black',
-	hobbies: ['golf', 'mathematics'],
-}, {
-	name: 'Jane Garcia',
-	age: 27,
-	hairColor: 'blonde',
-	hobbies: ['biology', 'medicine', 'gymnastics'],
-}];
-
-const products = [{
-	name: 'Flat-Screen TV',
-	price: '$300',
-	description: 'Huge LCD screen, a great deal',
-	rating: 4.5,
-}, {
-	name: 'Basketball',
-	price: '$10',
-	description: 'Just like the pros use',
-	rating: 3.8,
-}, {
-	name: 'Running Shoes',
-	price: '$120',
-	description: 'State-of-the-art technology for optimum running',
-	rating: 4.2,
-}];
+import { ProductInfo } from "./ProductInfo";
+import ResourceLoader from "./ResourceLoader";
+import UserInfo from "./UserInfo";
 
 function App() {
 	return (
     <>
-	  <Modal>
-		<LargeProductListItem product={products[0]} />
-	  </Modal>
+	  <ResourceLoader resourceUrl="/users/234" resourceName="user">
+        <UserInfo />
+      </ResourceLoader>
+      <ResourceLoader resourceUrl="/products/1234" resourceName="product">
+        <ProductInfo />
+      </ResourceLoader>
     </>
     );
 }
