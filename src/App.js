faa8e966c874d0986bb14f0d42d1f46785b9a5a4
Vihanga Pramudith <it20192082@my.sliit.lk>
2023-05-13 00:40:34 +0530

    custom hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import printProps from "./printProps";
-import UserInfo from "./UserInfo";
-import withUser from "./withUser";
-import UserInfoForm from "./UserInfoForm";
-
-const UserInfoWithLoader = withUser(UserInfo, "234")
-
+import { ProductInfo } from './ProductInfo';
+import { UserInfo } from './UserInfo';
 
 function App() {
-    return (
-        <UserInfoForm/>
-    );
+	return (
+		<>
+		<UserInfo userId="234" />
+		<ProductInfo productId="1234" />
+		</>
+	);
 }
 
 export default App;
--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,4 +1,22 @@
-const UserInfo = ({ user }) => {
+import axios from 'axios';
+import { useDataSource } from './useDataSource';
+import { useResource } from './useResource';
+import { useUser } from './useUser';
+
+const serverResource = resourceUrl => async () => {
+	const response = await axios.get(resourceUrl);
+	return response.data;
+};
+
+const localStorageResource = key => () => {
+	return localStorage.getItem(key);
+}
+
+export const UserInfo = ({ userId }) => {
+	// const user = useResource(`/users/${userId}`);
+	const user = useDataSource(serverResource(`/users/${userId}`));
+	const message = useDataSource(localStorageResource('message'));
+
 	const { name, age, hairColor, hobbies } = user || {};
 
 	return user ? (
@@ -11,7 +29,5 @@ const UserInfo = ({ user }) => {
 			{hobbies.map(hobby => <li key={hobby}>{hobby}</li>)}
 		</ul>
 		</>
-	) : <p>...Loading</p>;
+	) : <p>Loading...</p>;
 }
-
-export default UserInfo;
